e1a0a050ef3e4ccd7e9477f0a9420a5b979ee0dd
Johann Schneider <johann.schneider@MBP-von-Johann.fritz.box>
2022-04-14 10:41:58 +0200

    small changes and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Hello my Scrumbags :) {" <3"} </div>
+        CISUM
       </header>
+      <main>
+        hier entsteht ein webshop..
+      </main>
     </div>
   );
 }
