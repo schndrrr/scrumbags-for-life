5aa1d526357fd8bdda9c63f291996934df92db32
Johann Schneider <johann.schneider@MBP-von-Johann.fritz.box>
2022-04-14 10:19:51 +0200

    initial commit | first project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Hello my Scrumbags :) {" <3"} </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
