f7b126d98e0a5c62051cff6c9cdd9a853100a04c
Johann Schneider <johann.schneider@MacBook-Pro-von-Johann.local>
2022-04-27 12:39:55 +0200

    created first components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        CISUM
-      </header>
-      <main>
-        hier entsteht ein webshop..
-      </main>
+      {/* <Header></Header> */}
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        {/* <Footer></Footer> */}
     </div>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as styles from "/component.module.less";
+
+const Footer = () => {
+
+    return (
+        <div>
+            Footer
+        </div>
+    )
+}
+
+export default Footer;
