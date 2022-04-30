54e1ce23ef1ea6108c770e1d42762c9d66f34861
Tillman Koch <tillman.koch@dreebit.com>
2022-04-30 16:57:57 +0200

    Add menu components to Header

--- webapp/src/components/Header/index.tsx
+++ webapp/src/components/Header/index.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import "./components.modules.css";
 import CisumLogo from "../CisumLogo";
+import Search from "antd/es/input/Search";
+import MenuItem from "antd/es/menu/MenuItem";
+import Menu from "antd/es/menu"
+import {Button} from "antd";
 
 const Header = () => {
     return (
-        <div className={"container"}>
-            <CisumLogo height={60} fill={"#F4951B"}/>
-            <div className="buttonContainer">
-                <div className="button">
-                    Anmelden
-                </div>
-                <div className="button">
-                    Suchen
-                </div>
+        <div className={"header-container flex-row"}>
+            <CisumLogo height={60} fill={'#F4951E'}/>
+            <div className={"search-container"}>
+                <Search placeholder="input search text" enterButton maxLength={250} loading={false}/>
             </div>
+            <Menu mode="horizontal">
+                <MenuItem>
+                    Shop
+                </MenuItem>
+                <MenuItem>
+                    Warenkorb
+                </MenuItem>
+                <Button>
+                    Login
+                </Button>
+            </Menu>
         </div>
     )
 }
