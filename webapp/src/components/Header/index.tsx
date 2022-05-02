c38670442980ec888bf7730805ed54b8114ae73b
Tillman Koch <tillman.koch@dreebit.com>
2022-05-02 08:54:54 +0200

    Add Conditional Rendering Login/User

--- webapp/src/components/Header/index.tsx
+++ webapp/src/components/Header/index.tsx
@@ -2,56 +2,68 @@ import React from "react";
 import "./header.css";
 import CisumLogo from "../CisumLogo";
 import Search from "antd/es/input/Search";
-import { Menu, MenuProps, Button } from 'antd';
+import { Menu } from 'antd';
 import { ShoppingCartOutlined, LoginOutlined } from '@ant-design/icons';
 import {Link} from "react-router-dom";
 
-type MenuItem = Required<MenuProps>['items'][number];
-
 type Props = {
-    searchActive: boolean
+    searchActive: boolean,
+    loggedIn?: boolean
 }
 
-function getItem(
-    label: React.ReactNode,
-    key?: React.Key | null,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
-): MenuItem {
-    return {
-        key,
-        icon,
-        children,
-        label,
-    } as MenuItem;
-}
-
-const items: MenuProps['items']  = [
-
-    getItem(
-        <Button type='primary' href="../login" icon={<LoginOutlined/>}>
-            Login
-        </Button>,
-        'login',
-    ),
-    getItem(
-        <Link to={`../`}>Shop</Link>,
-        'shop',
-    ),
-    getItem(
-        <Link to={`../basket`}>Warenkorb</Link>,
-        'basket',
-        <ShoppingCartOutlined />,
-    ),
-    getItem(
-        <Link to={`../imprint`}>Impressum</Link>,
-        'imprint',
-    ),
-];
+// type MenuItem = Required<MenuProps>['items'][number];
+//
+// function getItem(
+//     label: React.ReactNode,
+//     key?: React.Key | null,
+//     icon?: React.ReactNode,
+//     children?: MenuItem[],
+// ): MenuItem {
+//     return {
+//         key,
+//         icon,
+//         children,
+//         label,
+//     } as MenuItem;
+// }
+//
+// const items: MenuProps['items']  = [
+//
+//     getItem(
+//         <Link to={`../login`}>Login</Link>,
+//         'login',
+//         <LoginOutlined/>
+//     ),
+//     getItem(
+//         <Link to={`../`}>Shop</Link>,
+//         'shop',
+//     ),
+//     getItem(
+//         <Link to={`../basket`}>Warenkorb</Link>,
+//         'basket',
+//         <ShoppingCartOutlined />,
+//     ),
+//     getItem(
+//         <Link to={`../imprint`}>Impressum</Link>,
+//         'imprint',
+//     ),
+// ];
 
 const Header = (props:Props) => {
 
     return (
+        // <div className={"header-container flex-row"}>
+        //     <CisumLogo fill={'#F4951E'}/>
+        //     {props.searchActive &&
+        //     <div className={"search-container"}>
+        //         <Search placeholder="Finde deinen Lieblingssong" enterButton maxLength={250} loading={false}/>
+        //     </div>
+        //     }
+        //     <div>
+        //         <Menu className={'header-menu'} mode="horizontal" items={items} defaultSelectedKeys={[]} disabledOverflow={true}/>
+        //     </div>
+        // </div>
+
         <div className={"header-container flex-row"}>
             <CisumLogo fill={'#F4951E'}/>
             {props.searchActive &&
@@ -59,7 +71,28 @@ const Header = (props:Props) => {
                 <Search placeholder="Finde deinen Lieblingssong" enterButton maxLength={250} loading={false}/>
             </div>
             }
-            <Menu className={'header-menu'} mode="horizontal" items={items} defaultSelectedKeys={[]} disabledOverflow={true}/>
+            <div>
+
+                <Menu className={'header-menu'} mode="horizontal" defaultSelectedKeys={[]} disabledOverflow={true}>
+                    {props.loggedIn ?
+                    <Menu.Item key={'user'}>
+                        <Link to={`../user`}>Benutzer</Link>
+                    </Menu.Item> :
+                    <Menu.Item key={'login'} icon={<LoginOutlined/>}>
+                    <Link to={`../login`}>Login</Link>
+                    </Menu.Item>}
+                    <Menu.Item key={'shop'}>
+                        <Link to={`../`}>Shop</Link>
+                    </Menu.Item>
+                    <Menu.Item key={'basket'} icon={<ShoppingCartOutlined />}>
+                        <Link to={`../basket`}>Warenkorb</Link>
+                    </Menu.Item>
+                    <Menu.Item key={'imprint'}>
+                        <Link to={`../imprint`}>Imprint</Link>
+                    </Menu.Item>
+
+                </Menu>
+            </div>
         </div>
     )
 }
