1864b238b4a8c82eb95e4c86963d488d4af0c878
Tillman Koch <tillman.koch@dreebit.com>
2022-05-02 12:54:57 +0200

    Footer Init

--- webapp/src/components/Footer/index.tsx
+++ webapp/src/components/Footer/index.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-
+import './footer.css';
+import CisumLogo from "../CisumLogo";
+import {Link} from "react-router-dom";
 
 const Footer = () => {
 
     return (
-        <div>
-            Footer
+        <div className={'footer-container flex-row'}>
+            <CisumLogo fill={'#F4951E'}/>
+            <div className={'footer-content flex-column'} >
+                Musikplatz, 012345 Musikstadt <br/>
+                info@cisum.de <br/>
+                Über uns
+            </div>
+            <div className={'footer-content flex-column'} >
+                <Link to={`../imprint`}>Datenschutz</Link>
+                <Link to={`../imprint`}>Impressum</Link>
+                <Link to={`../imprint`}>Wiederrufsbelehrung</Link>
+            </div>
         </div>
     )
 }
