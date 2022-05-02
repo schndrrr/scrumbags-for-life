fa5d64b182cacd2c8e46e1b1902cf14e9351d19e
Tillman Koch <tillman.koch@dreebit.com>
2022-05-02 14:35:10 +0200

    footer finished

--- webapp/src/components/Footer/index.tsx
+++ webapp/src/components/Footer/index.tsx
@@ -6,17 +6,23 @@ import {Link} from "react-router-dom";
 const Footer = () => {
 
     return (
-        <div className={'footer-container flex-row'}>
-            <CisumLogo fill={'#F4951E'}/>
-            <div className={'footer-content flex-column'} >
-                Musikplatz, 012345 Musikstadt <br/>
-                info@cisum.de <br/>
-                Über uns
+        <div className={'footer-container flex'}>
+            <div className={'flex flex-self-end footer-copy'}>
+                <CisumLogo fill={'#F4951E'} width={'80px'}/>
+                <div className={'flex flex-self-end'}>&copy; 2022 </div>
             </div>
-            <div className={'footer-content flex-column'} >
-                <Link to={`../imprint`}>Datenschutz</Link>
-                <Link to={`../imprint`}>Impressum</Link>
-                <Link to={`../imprint`}>Wiederrufsbelehrung</Link>
+
+            <div className={'flex-row footer-content-container'}>
+                <div className={'text-center footer-content flex-column'} >
+                    Musikplatz, 012345 Musikstadt <br/>
+                    info@cisum.de <br/>
+                    Über uns
+                </div>
+                <div className={'footer-content flex-column footer-content-right'} >
+                    <Link to={`../imprint`}>Datenschutz</Link>
+                    <Link to={`../imprint`}>Impressum</Link>
+                    <Link to={`../imprint`}>Wiederrufsbelehrung</Link>
+                </div>
             </div>
         </div>
     )
