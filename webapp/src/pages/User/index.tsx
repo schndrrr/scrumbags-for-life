aa52b65f7968a8230a34788b5397ce20dd3a3711
Tillman Koch <tillman.koch@dreebit.com>
2022-05-09 10:07:08 +0200

    Add User Page and Component

--- webapp/src/pages/User/index.tsx
+++ webapp/src/pages/User/index.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import UserDataOverview from "../../components/UserDataOverview";
+import PaymentPicker from "../../components/PaymentPicker";
+import OrderHistory from "../../components/OrderHistory";
 
 const User = () => {
 
     return (
-        <>
-            <Header searchActive={false} loggedIn={true}/>
-
-            <Footer />
-        </>
+            <section className={'content'}>
+                <Header searchActive={false} loggedIn={true}/>
+                <div className={'basket-container flex'}>
+                    <OrderHistory/>
+                    <div className={'flex-col flex-space-between'}>
+                        <div className={'basket-sidebar flex-col'}>
+                            <UserDataOverview/>
+                            <PaymentPicker/>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </section>
     )
 }
 
