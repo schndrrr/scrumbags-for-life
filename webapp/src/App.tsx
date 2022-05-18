3ae865865ce706dde3b3aab34830b69f25b0f596
Tillman Koch <tillman.koch@dreebit.com>
2022-05-18 10:33:13 +0200

    make routing conditional

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  useRoutes,
-} from "react-router-dom";
+import {BrowserRouter as Router, useRoutes, Route, Routes, useNavigate} from "react-router-dom";
 import "./main.css";
 import Home from "./pages/Home";
 import Basket from "./pages/Basket";
@@ -14,27 +11,21 @@ import Imprint from "./pages/Imprint";
 import DataPrivacy from "./pages/DataPrivacy";
 import Revocation from "./pages/Revocation";
 
-const AppRoute = () => {
-  let routes = useRoutes([
-    {path: "/", element: <Home /> },
-    {path: "/basket", element: <Basket/>},
-    {path: "/login", element: <Login/>},
-    {path: "/user", element: <User/>},
-    {path: "/ordersuccess", element: <OrderSuccess/>},
-    {path: "/payment", element: <PayPal/>},
-    {path: "/imprint", element: <Imprint/>},
-    {path: "/privacy", element: <DataPrivacy/>},
-    {path: "/revocation", element: <Revocation/>},
-
-    // ...
-  ]);
-  return routes;
-};
-
 const App = () => {
-  return (
+   return (
       <Router>
-        <AppRoute />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/basket" element={<Basket />}/>
+          <Route path="/login" element={<Login />}/>
+          {/*auto redirectiing to login if not logged in*/}
+          <Route path="/user" element={localStorage.getItem('user') ? <User /> : <Login/>}/>
+          <Route path="/ordersuccess" element={<OrderSuccess />}/>
+          <Route path="/payment" element={<PayPal />}/>
+          <Route path="/imprint" element={<Imprint />}/>
+          <Route path="/privacy" element={<DataPrivacy />}/>
+          <Route path="/revocation" element={<Revocation />}/>
+        </Routes>
       </Router>
   );
 };
