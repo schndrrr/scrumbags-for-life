c3708f3e1f289c6996c2b0efc442164f04bcc6a4
ilselohr <ilse@ostec.de>
2022-05-23 13:40:22 +0200

    make login form beautiful again bug fix!

--- webapp/src/pages/Login/index.tsx
+++ webapp/src/pages/Login/index.tsx
@@ -8,9 +8,9 @@ const Login = () => {
 
     return (
         <div className={"login-background-design"}>
-            <div className={'banner-container-logo'}>
-                <img src={CisumBannerLogo} height={450} width={800} className={'banner-container-logo-left'}/>
-                <img src={CisumBannerLogo} height={450} width={800} className={'banner-container-logo-right'}/>
+            <div className={'bannerlogin-container-logo'}>
+                <img src={CisumBannerLogo} height={450} width={800} className={'bannerlogin-container-logo-left'}/>
+                <img src={CisumBannerLogo} height={450} width={800} className={'bannerlogin-container-logo-right'}/>
             </div>
         <LoginForm/>
         </div>
