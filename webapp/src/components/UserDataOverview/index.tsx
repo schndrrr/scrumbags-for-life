82478883e09545e1a5ec44170ac1e1341d3fe966
Tillman Koch <tillman.koch@dreebit.com>
2022-05-23 16:03:14 +0200

    UserDataOverview Parameter Fix

--- webapp/src/components/UserDataOverview/index.tsx
+++ webapp/src/components/UserDataOverview/index.tsx
@@ -10,7 +10,7 @@ const UserDataOverview = () => {
     return (
         <div className={'user-data-container flex-col'}>
             <Divider orientation={'left'} className={'data-field-label'}>Kunde</Divider>
-            {user && <div className={'data-field'}>{user.username}</div>}
+            {user && <div className={'data-field'}>{user.firstname} {user.lastname}</div>}
             <Divider orientation={'left'} className={'data-field-label'}>Anschrift</Divider>
             {user && <div className={'data-field'}>{user.street} {user.streetNr}</div>}
             {user && <div className={'data-field'}>{user.postalCode} {user.city}</div>}
